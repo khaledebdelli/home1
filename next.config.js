97933a76c633ca379c4352fa91fd8b60d8d4488c
khaledebdelli <khaled88ebdelli@gmail.com>
2022-04-19 16:53:05 +0200

    fix images next domain

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  // images: {
-  //   domains: ['assets.example.com'],
-  // },
+  images: {
+    domains: ['avatars.githubusercontent.com'],
+  },
 }

e4a6d3943d74c3cd0b04ae6ef255fa7918d7df1f
khaledebdelli <khaled88ebdelli@gmail.com>
2022-04-19 16:55:12 +0200

    add thumbnail component

--- components/Thumbnail.tsx
+++ components/Thumbnail.tsx
@@ -1,27 +1,38 @@
-import Image from 'next/image'
-import { Project } from '../typings'
+import Link from 'next/link'
+import { HiOutlineExternalLink } from 'react-icons/hi'
+import { Repository } from '../typings'
 
 interface Props {
-  project: Project
+  repository: Repository
 }
 
-function Thumbnail({ project }: Props) {
-
+function Thumbnail({ repository }: Props) {
   return (
     <div
-      className={`relative h-28 min-w-[180px] cursor-pointer transition duration-200 ease-out md:h-36 md:min-w-[260px] md:hover:scale-105`}
-    //   onClick={() => {
-    //     setCurrentMovie(movie)
-    //     setShowModal(true)
-    //   }}
+      className={`relative min-w-[220px] cursor-pointer overflow-y-hidden transition duration-200 ease-out md:h-36 md:min-w-[260px] md:hover:scale-105`}
     >
-      <Image
-        src={`https://image.tmdb.org/t/p/w500${
-          project.backdrop_path
-        }`}
-        className="rounded-sm object-cover md:rounded"
-        layout="fill"
-      />
+      <div className="overflow-x-hidden">
+        <div className="rounded overflow-hidden shadow-lg">
+          <div className="px-6 flex space-x-3">
+            <img
+              className="rounded object-cover h-10 w-10"
+              src={repository.owner.avatar_url}
+              alt="user avatar"
+            />
+            <div className="text-amber-700 font-bold text-sm mb-4">
+              <Link href={repository.html_url} passHref={true}>
+                <a className="flex space-x-4" target="_blank" rel="noreferrer">
+                  <p>{repository.name}</p>
+                  <HiOutlineExternalLink color='white' min={15} size={15} />
+                </a>
+              </Link>
+            </div>
+          </div>
+          <p className="text-white-700 text-xs p-2 truncate text-ellipsis ...">
+            {repository.description && repository.description}
+          </p>
+        </div>
+      </div>
     </div>
   )
 }

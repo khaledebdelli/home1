ae33145fe543518d0db1f9d12b6b4f3fecf14b8a
khaledebdelli <khaled88ebdelli@gmail.com>
2022-05-10 13:42:26 +0200

    test api key firebase

--- firebase.ts
+++ firebase.ts
@@ -9,12 +9,12 @@ import { getAuth } from 'firebase/auth'
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyAxblaYL6DKhArv7g1sCU4WtgP7yb-959A',
-  authDomain: 'my-protfolio-1.firebaseapp.com',
-  projectId: 'my-protfolio-1',
-  storageBucket: 'my-protfolio-1.appspot.com',
-  messagingSenderId: '559030080996',
-  appId: '1:559030080996:web:dea20343726978aeab63f1',
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGIN_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 }
 
 // Initialize Firebase

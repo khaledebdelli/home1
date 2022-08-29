cdd97ac16072081a1c28f4e432c18a93162f0daf
aassilatn <ali.assila.it@gmail.com>
2022-08-29 16:55:44 +0100

    fix account.tsx error

--- pages/account.tsx
+++ pages/account.tsx
@@ -62,13 +62,13 @@ function Account({ products }: Props) {
         <div className="mt-6 grid grid-cols-1 gap-x-4 border px-4 py-4 md:grid-cols-4 md:border-x-0 md:border-t md:border-b-0 md:px-0 md:pb-0">
           <h4 className="text-lg text-[gray]">Plan Details</h4>
           {/* Find the current plan */}
-          <div className="col-span-2 font-medium">
+          {/* <div className="col-span-2 font-medium">
             {
               products.filter(
                 (product) => product.id === subscription?.product
               )[0]?.name
             }
-          </div>
+          </div> */}
           <p
             className="cursor-pointer text-blue-500 hover:underline md:text-right"
           >

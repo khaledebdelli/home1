d0dae31bf784d1bebf999c032ff4c84ad7e75d3a
khaledebdelli <khaled88ebdelli@gmail.com>
2022-09-21 14:29:59 +0100

    add UnderConstruction illustrator

---
+++ pages/about.tsx
@@ -0,0 +1,10 @@
+import Head from 'next/head'
+import Header from '../components/Header'
+import UnderConstruction from "../assets/under_construction.svg";
+const About = () => {
+  return (
+    <UnderConstruction />
+  )
+}
+
+export default About
